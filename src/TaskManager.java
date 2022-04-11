eea17ac373524b5afe76b205c5726bbefb298d01
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-04-11 22:09:33 +0200

    Initial commit

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -9,6 +9,8 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import static pl.coderslab.ConsoleColors.*;
+
 public class TaskManager {
 
     public static void main(String[] args) throws IOException {
@@ -16,13 +18,30 @@ public class TaskManager {
         String fileName="tasks.csv";
         String[][] tasks = fileToString(fileName);
 
-        System.out.println(Arrays.deepToString(tasks));
+        printMenu();
+        Scanner scannerMenu = new Scanner(System.in);
+        String userSelection = scannerMenu.nextLine();
 
-        printList(tasks);
+        while(!userSelection.equals("exit")){
+                switch (userSelection){
+                    case "add":
+    //                    add option
+                        break;
+                    case "remove":
+                        tasks = removeTask(tasks);
+                        break;
+                    case "list":
+                        printList(tasks);
+                        break;
+                    default:
+                        System.out.println("Incorrect selection");
+                }
 
-        tasks = removeTask(tasks);
-        printList(tasks);
+            printMenu();
+            userSelection= scannerMenu.next();
+        }
 
+        saveFile(tasks, fileName);
     }
 
     public static String[][] fileToString(String filename) throws IOException {
@@ -79,6 +98,32 @@ public class TaskManager {
         taskNumber=scanRemove.nextInt();
 
         return ArrayUtils.removeElement(tasks, tasks[taskNumber]);
+    }
+
+    public static void printMenu(){
+        String[] menuList=new String[]{"add", "remove", "list", "exit"};
+        System.out.println(BLUE + "Please select an option" + RESET);
+        for(String element : menuList){
+            System.out.println(element);
+        }
+    }
+
+    public static void saveFile(String[][] tasksList, String filename) {
+
+        String[] output = new String[tasksList.length];
+        for(int i=0; i<tasksList.length; i++){
+            output[i] = StringUtils.join(tasksList[i], ", ");
+        }
+        String outputString = StringUtils.join(output, "\n");
+
+        Path filePath = Paths.get("src/"+filename);
+
+        try {
+            Files.writeString(filePath, outputString);
+        } catch (IOException ex) {
+            System.out.println("Nie można zapisać pliku.");
+        }
+
     }
 
 }
